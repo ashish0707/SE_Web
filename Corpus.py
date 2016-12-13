65701490a650d53acc1607f192b6067c03c13314
ashish0707 <saashaa0707@gmail.com>
2016-12-13 18:45:03 -0500

    changes files to index to 1000

--- Corpus.py
+++ Corpus.py
@@ -59,7 +59,7 @@ class NGramGenerator:
         docId = 1
         for filename in os.listdir(folder):
 
-            if docId > 100:
+            if docId > 1000:
                 break
             abs_fileName = os.path.join(folder, filename)
             for word in open(abs_fileName).read().split():
