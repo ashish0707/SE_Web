b62f1548e9d43916eebbde1b4468c3c2d8b2506e
ashish0707 <saashaa0707@gmail.com>
2016-12-13 19:56:58 -0500

    added host-0.0.0.0 in app.py

--- app.py
+++ app.py
@@ -66,5 +66,10 @@ def search():
 
     # return "new query"
 
+
+def application(env, start_response):
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    return ["Hello!"]
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
